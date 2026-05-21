02a0e6c53437ba63ccac13c41947ae1d5cb89c11
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-05-21 16:01:50 +0700

    auto(17d3c339): What's our OpenSubtitles retry strategy?
    File: test/support/host_agent.ex
    Session: 17d3c339-b89b-4f08-b62e-dd3f5900b8d8
    TranscriptPath: /Users/elimydlarz/.claude/projects/-Users-elimydlarz-projects-gralkor-and-friends-jido-watch/17d3c339-b89b-4f08-b62e-dd3f5900b8d8.jsonl

--- test/support/host_agent.ex
+++ test/support/host_agent.ex
@@ -29,7 +29,6 @@ defmodule JidoWatch.Test.Support.HostAgent do
     connection = Keyword.get(opts, :connection, :unconnected)
     watermark = Keyword.get(opts, :watermark)
     poll_interval_minutes = Keyword.get(opts, :poll_interval_minutes)
-    transient_retry_delay_ms = Keyword.get(opts, :transient_retry_delay_ms)
 
     plugin_state =
       %{
@@ -42,7 +41,6 @@ defmodule JidoWatch.Test.Support.HostAgent do
         watermark: watermark
       }
       |> maybe_put(:poll_interval_minutes, poll_interval_minutes)
-      |> maybe_put(:transient_retry_delay_ms, transient_retry_delay_ms)
 
     AgentServer.start_link(
       agent: __MODULE__,
