fdb26392a3b40cc7edc59167d8eb4733c0b51ea1
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-05-26 21:26:33 +0700

    auto(1db08c1d): How goes the consuming agent explore what the user has watched recently,
    File: test/adapter/trakt_in_memory_test.exs
    Session: 1db08c1d-93d6-4c59-848a-47f87627c2e1
    TranscriptPath: /Users/elimydlarz/.claude/projects/-Users-elimydlarz-projects-gralkor-and-friends-jido-watch/1db08c1d-93d6-4c59-848a-47f87627c2e1.jsonl

--- test/adapter/trakt_in_memory_test.exs
+++ test/adapter/trakt_in_memory_test.exs
@@ -19,5 +19,20 @@ defmodule JidoWatch.Adapter.TraktInMemoryTest do
   defp setup_for(:recent_watches_unauthorized),
     do: TraktInMemory.start!(unauthorized_access_tokens: ["tok-dead"])
 
+  defp setup_for(:watched_shows_valid), do: TraktInMemory.start!(watched_shows: [%{"plays" => 1}])
+
+  defp setup_for(:watched_shows_unauthorized),
+    do: TraktInMemory.start!(unauthorized_access_tokens: ["tok-dead"])
+
+  defp setup_for(:watched_movies_valid), do: TraktInMemory.start!(watched_movies: [%{"plays" => 1}])
+
+  defp setup_for(:watched_movies_unauthorized),
+    do: TraktInMemory.start!(unauthorized_access_tokens: ["tok-dead"])
+
+  defp setup_for(:stats_valid), do: TraktInMemory.start!(stats: %{"episodes" => %{"watched" => 1}})
+
+  defp setup_for(:stats_unauthorized),
+    do: TraktInMemory.start!(unauthorized_access_tokens: ["tok-dead"])
+
   TraktClientContract.run()
 end
